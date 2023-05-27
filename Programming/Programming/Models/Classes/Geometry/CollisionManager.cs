6779fca3079cf3aa6c13bb2f35d23a88f1a853e3
Andrey Shchedrin <shedrin2001@mail.ru>
2023-05-27 13:31:23 +0700

    Change IsCollision method

--- Programming/Programming/Models/Classes/Geometry/CollisionManager.cs
+++ Programming/Programming/Models/Classes/Geometry/CollisionManager.cs
@@ -4,8 +4,23 @@
     {
         public static bool IsCollision(Rectangle rectangle1, Rectangle rectangle2)
         {
-            return Math.Abs(rectangle1.Center.X - rectangle2.Center.X) < rectangle1.Lenght / 2 + rectangle2.Lenght / 2
-                && Math.Abs(rectangle1.Center.Y - rectangle2.Center.Y) < rectangle1.Width / 2 + rectangle2.Width / 2;
+            double halfWidth1 = rectangle1.Width / 2;
+            double halfHeight1 = rectangle1.Height / 2;
+            double halfWidth2 = rectangle2.Width / 2;
+            double halfHeight2 = rectangle2.Height / 2;
+
+            double centerX1 = rectangle1.Center.X + halfWidth1;
+            double centerY1 = rectangle1.Center.Y + halfHeight1;
+            double centerX2 = rectangle2.Center.X + halfWidth2;
+            double centerY2 = rectangle2.Center.Y + halfHeight2;
+
+            double deltaX = Math.Abs(centerX1 - centerX2);
+            double deltaY = Math.Abs(centerY1 - centerY2);
+
+            double intersectX = halfWidth1 + halfWidth2;
+            double intersectY = halfHeight1 + halfHeight2;
+
+            return deltaX < intersectX && deltaY < intersectY;
         }
 
         public static bool IsCollision(Ring ring1, Ring ring2)
