c251fd4b49f438fb8e7d73b9692a22c1dc8263ca
Polina <Polina@LAPTOP-IJHS1JNV>
2023-10-09 23:11:35 +0700

    .

---
+++ scr/ObjectOrientedPractics/ObjectOrientedPractics/View/Controls/AddressUserControl.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ObjectOrientedPractics.View.Controls
+{
+    partial class AddressUserControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            IndexTextBox = new TextBox();
+            CountryTextBox = new TextBox();
+            StreetTextBox = new TextBox();
+            BuildingTextBox = new TextBox();
+            ApartmentTextBox = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            CityTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(13, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Delivery Address";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(13, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Post Index:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(13, 91);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Country:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(13, 133);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Street:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(13, 176);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Building:";
+            // 
+            // IndexTextBox
+            // 
+            IndexTextBox.Location = new Point(108, 44);
+            IndexTextBox.Name = "IndexTextBox";
+            IndexTextBox.Size = new Size(169, 27);
+            IndexTextBox.TabIndex = 5;
+            // 
+            // CountryTextBox
+            // 
+            CountryTextBox.Location = new Point(108, 88);
+            CountryTextBox.Name = "CountryTextBox";
+            CountryTextBox.Size = new Size(223, 27);
+            CountryTextBox.TabIndex = 6;
+            // 
+            // StreetTextBox
+            // 
+            StreetTextBox.Location = new Point(108, 130);
+            StreetTextBox.Name = "StreetTextBox";
+            StreetTextBox.Size = new Size(618, 27);
+            StreetTextBox.TabIndex = 7;
+            // 
+            // BuildingTextBox
+            // 
+            BuildingTextBox.Location = new Point(108, 173);
+            BuildingTextBox.Name = "BuildingTextBox";
+            BuildingTextBox.Size = new Size(169, 27);
+            BuildingTextBox.TabIndex = 8;
+            // 
+            // ApartmentTextBox
+            // 
+            ApartmentTextBox.Location = new Point(454, 173);
+            ApartmentTextBox.Name = "ApartmentTextBox";
+            ApartmentTextBox.Size = new Size(125, 27);
+            ApartmentTextBox.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(365, 176);
+            label6.Name = "label6";
+            label6.Size = new Size(83, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Apartment:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(385, 91);
+            label7.Name = "label7";
+            label7.Size = new Size(37, 20);
+            label7.TabIndex = 11;
+            label7.Text = "City:";
+            // 
+            // CityTextBox
+            // 
+            CityTextBox.Location = new Point(454, 88);
+            CityTextBox.Name = "CityTextBox";
+            CityTextBox.Size = new Size(272, 27);
+            CityTextBox.TabIndex = 12;
+            // 
+            // AddressUserControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(CityTextBox);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(ApartmentTextBox);
+            Controls.Add(BuildingTextBox);
+            Controls.Add(StreetTextBox);
+            Controls.Add(CountryTextBox);
+            Controls.Add(IndexTextBox);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "AddressUserControl";
+            Size = new Size(743, 208);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox IndexTextBox;
+        private TextBox CountryTextBox;
+        private TextBox StreetTextBox;
+        private TextBox BuildingTextBox;
+        private TextBox ApartmentTextBox;
+        private Label label6;
+        private Label label7;
+        private TextBox CityTextBox;
+    }
+}
